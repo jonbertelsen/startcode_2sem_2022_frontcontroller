1bbdd2107f545ff8e6f8daa661fdcda5fb5991b1
Jon Bertelsen <jobe@cphbusiness.dk>
2022-04-23 20:52:00 +0200

    update tests to use facade

--- src/main/java/dat/startcode/model/services/UserFacade.java
+++ src/main/java/dat/startcode/model/services/UserFacade.java
@@ -5,11 +5,19 @@ import dat.startcode.model.exceptions.DatabaseException;
 import dat.startcode.model.persistence.ConnectionPool;
 import dat.startcode.model.persistence.UserMapper;
 
+import javax.xml.crypto.Data;
+
 public class UserFacade
 {
     public static User login(String username, String password, ConnectionPool connectionPool) throws DatabaseException
     {
         UserMapper userMapper = new UserMapper(connectionPool);
         return userMapper.login(username, password);
+    }
+
+    public static User createUser(String username, String password, String role, ConnectionPool connectionPool) throws DatabaseException
+    {
+        UserMapper userMapper = new UserMapper(connectionPool);
+        return userMapper.createUser(username, password, role);
     }
 }
--- src/test/java/dat/startcode/persistence/UserMapperTest.java
+++ src/test/java/dat/startcode/persistence/UserMapperTest.java
@@ -4,6 +4,7 @@ import dat.startcode.model.entities.User;
 import dat.startcode.model.exceptions.DatabaseException;
 import dat.startcode.model.persistence.ConnectionPool;
 import dat.startcode.model.persistence.UserMapper;
+import dat.startcode.model.services.UserFacade;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -21,12 +22,10 @@ class UserMapperTest
     private final static String URL = "jdbc:mysql://localhost:3306/startcode_test?serverTimezone=CET&allowPublicKeyRetrieval=true&useSSL=false";
 
     private static ConnectionPool connectionPool;
-    private static UserMapper userMapper;
 
     @BeforeAll
     public static void setUpClass() {
             connectionPool = new ConnectionPool(USER, PASSWORD, URL);
-            userMapper = new UserMapper(connectionPool);
     }
 
     @BeforeEach
@@ -61,27 +60,27 @@ class UserMapperTest
     void login() throws DatabaseException
     {
         User expectedUser = new User("user","1234","user");
-        User actualUser = userMapper.login("user","1234");
+        User actualUser = UserFacade.login("user","1234", connectionPool);
         assertEquals(expectedUser, actualUser);
     }
 
     @Test
     void invalidPasswordLogin() throws DatabaseException
     {
-        assertThrows(DatabaseException.class, () -> userMapper.login("user","123"));
+        assertThrows(DatabaseException.class, () -> UserFacade.login("user","123", connectionPool));
     }
 
     @Test
     void invalidUserNameLogin() throws DatabaseException
     {
-        assertThrows(DatabaseException.class, () -> userMapper.login("bob","1234"));
+        assertThrows(DatabaseException.class, () -> UserFacade.login("bob","1234", connectionPool));
     }
 
     @Test
     void createUser() throws DatabaseException
     {
-        User newUser = userMapper.createUser("jill", "1234", "user");
-        User logInUser = userMapper.login("jill","1234");
+        User newUser = UserFacade.createUser("jill", "1234", "user", connectionPool);
+        User logInUser = UserFacade.login("jill","1234", connectionPool);
         User expectedUser = new User("jill", "1234", "user");
         assertEquals(expectedUser, newUser);
         assertEquals(expectedUser, logInUser);
